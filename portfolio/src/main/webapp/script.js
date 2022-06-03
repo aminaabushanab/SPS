a2038f11243824b5c59c106b0ece0f5b5b5602cb
aminaabushanab <aa4869@columbia.edu>
2022-06-03 12:57:05 +0000

    First portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,32 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random fact to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomFact() {
+  const facts =
+      ['I had 6 perroquets!', 'I drank 2 liters of Diet Coke daily throughout high school!', 'I am currently learning how to mix music!', 'My favourite type of shows are sitcoms!'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
 }
+
+function addRandomImage() {
+    // The images directory contains 6 images, so generate a random index between
+    // 1 and 6.
+    const imageIndex = Math.floor(Math.random() * 6) + 1;
+    const imgUrl = 'images/kit-' + imageIndex + '.jpeg';
+  
+    const imgElement = document.createElement('img');
+    imgElement.src = imgUrl;
+  
+    const imageContainer = document.getElementById('random-image-container');
+    // Remove the previous image.
+    imageContainer.innerHTML = '';
+    imageContainer.appendChild(imgElement);
+  }
+  
